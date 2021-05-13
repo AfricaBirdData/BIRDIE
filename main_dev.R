07bd00d9fcda8761ff931f64dd0906da610cdd21
patchcervan <francerper@gmail.com>
2021-05-13 11:26:31 +0200

    Package creation

---
+++ main_dev.R
@@ -0,0 +1,67 @@
+# 12-5-2021
+
+# Script to keep track of package development
+
+# DO NOT RUN EVERYTHING AGAIN
+
+# If you want to add a function copy and paste a function block
+# at the end of the script (before the install part) and run
+# that section that you just pasted.
+
+rm(list = ls())
+
+library(devtools)
+library(testthat)
+
+
+# Create a package structure
+create_package("D:/Documentos/mis_trabajos/Academic/BIRDIE/BIRDIE")
+
+# Add license
+use_mit_license("BIRDIE Development Team")
+
+# Remember to edit the DESCRIPTION file
+
+
+
+# Imports -----------------------------------------------------------------
+
+# Import pipe
+use_pipe()
+
+# Import packages
+use_package("dplyr")
+
+
+# Function Example --------------------------------------------------
+
+# Add function
+use_r("Example")
+
+# test locally
+load_all()
+
+Example("argument")
+
+# Add documentation
+# Add ROxygen skeleton manually
+document()
+
+check()
+
+# Add tests
+
+use_testthat()
+
+use_test()
+
+test()
+
+
+
+
+# Install -----------------------------------------------------------------
+
+install()
+
+
