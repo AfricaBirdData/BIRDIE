7d77c7612e84a1cdbfd2a419186513ad5c90301f
patchcervan <francerper@gmail.com>
2021-05-17 14:10:43 +0200

    Re-organized code (migrants?)

--- analysis/scripts/1_extract_data.R
+++ analysis/scripts/1_extract_data.R
@@ -1,3 +1,7 @@
+# 17-05-2021
+
+# In this script we download CWAC data for the Barberspan
+
 library(CWAC)
 library(tidyverse)
 
@@ -14,15 +18,31 @@ loc_code <- listCwacSites("North West") %>%
 # Extract location cards
 cards <- listCwacCards(loc_code)
 
-# Download cards
-counts <- map_df(cards$Card, ~getCwacSurvey(.x)[["records"]])
+# Download surveys based on location cards
+surveys <- map(cards$Card, ~getCwacSurvey(.x))
 
-# Download metadata
-info <- map_df(cards$Card, ~getCwacSurvey(.x)[["summary"]])
+
+# Join counts with survey metadata ----------------------------------------
+
+# Extract counts
+counts <- map_df(surveys, ~.x[["records"]])
+
+# Extract survey metadata
+info <- map_df(surveys, ~.x[["summary"]])
+
+
+
+# Migratory status?? (can we find this somewhere?)
+searchCwacTerm("SpeciesList")
+
+
 
 # Combine data and info
 counts <- left_join(counts, info, by = c("card" = "Card"))
 
-# Save data
+
+
+# Save data ---------------------------------------------------------------
+
 saveRDS(counts, "analysis/data/barberspan.rds")
 
