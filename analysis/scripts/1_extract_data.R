8d7035f2a8bb8ca2d8eccfa4a834a6d75924ff21
patchcervan <francerper@gmail.com>
2021-05-19 16:29:47 +0200

    Added migratory status to data extraction (preliminary)

--- analysis/scripts/1_extract_data.R
+++ analysis/scripts/1_extract_data.R
@@ -4,11 +4,16 @@
 
 library(CWAC)
 library(tidyverse)
+devtools::load_all() # To load BIRDIE package functions
 
 rm(list = ls())
 
 
 # Extract data ------------------------------------------------------------
+
+# We will need the migratory status of the different species later, so we need
+# to load these data
+bird_ids <- read.csv("analysis/data/barberspanIDs.csv")
 
 # Find location code
 loc_code <- listCwacSites("North West") %>%
@@ -25,12 +30,22 @@ surveys <- map(cards$Card, ~getCwacSurvey(.x))
 # Join counts with survey metadata ----------------------------------------
 
 # Extract counts
-counts <- map_df(surveys, ~.x[["records"]])
+counts <- map(surveys, ~.x[["records"]])
+
+# Check that all surveys have the same fields
+BIRDIE::checkAllIdentical(lapply(counts, names))
+
+# If TRUE bind data frame
+counts <- do.call("rbind", counts)
 
 # Extract survey metadata
-info <- map_df(surveys, ~.x[["summary"]])
+info <- map(surveys, ~.x[["summary"]])
 
+# Check that all summaries have the same fields
+BIRDIE::checkAllIdentical(lapply(info, names))
 
+# Use data.table to bind dataframes with different fields
+info <- data.table::rbindlist(info, fill = TRUE)
 
 # Migratory status?? (can we find this somewhere?)
 searchCwacTerm("SpeciesList")
@@ -38,8 +53,10 @@ searchCwacTerm("SpeciesList")
 
 
 # Combine data and info
-counts <- left_join(counts, info, by = c("card" = "Card"))
+counts <- full_join(counts, info, by = c("card" = "Card"))
 
+# Add migratory status
+counts <- left_join(counts, bird_ids, by = c("spp" = "id"))
 
 
 # Save data ---------------------------------------------------------------
