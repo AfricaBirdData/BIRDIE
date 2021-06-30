aef6a6a162453cac44f3978cd7732225a1b17f35
patchcervan <francerper@gmail.com>
2021-06-30 18:46:43 +0200

    Added occupancy observations animation

--- comms/JRS_Biodiversity_Seminar/jrs_seminar.R
+++ comms/JRS_Biodiversity_Seminar/jrs_seminar.R
@@ -11,7 +11,7 @@ set.seed(43562345)
 
 # set.seed(452345)
 
-# Create temporal frame ---------------------------------------------------
+# Create temporal frame for abundance -------------------------------------
 
 # Number of observations
 N <- 100
@@ -91,3 +91,98 @@ df %>%
     transition_reveal(t)
 
 anim_save("comms/output/abund_obs_anim.gif")
+
+
+# Create temporal frame for abundance -------------------------------------
+
+# Number of observations
+N <- 100
+
+# Preliminary time increments(months)
+dt <- ceiling(rexp(N-1, 0.5))
+
+# Times
+t <- c(0, cumsum(dt))
+
+# Create regular observation times
+t_obs <- seq(20, max(t) - 20, length.out = 10)
+
+# Integrate with the process simulation times
+t <- sort(c(t[!t %in% t_obs], t_obs))
+
+# Recalculate dt
+dt <- lead(t) - t
+
+
+# Create a time series of an occupancy process ----------------------------
+
+set.seed(4526)
+
+# Occupancy probabilities vector in logit
+logitp <- vector("numeric", length = length(t))
+
+# Initial probability (logit)
+logitp[1] <- -1
+
+# Diffusion standard deviation
+sigma <- 0.01
+
+# Change standard deviation
+sig_beta <- 0.005
+
+# Initial probability change
+beta <- -0.01
+
+# Generate abundance time series
+for(i in seq_along(logitp[-1])){
+    logitp[i+1] <- logitp[i] + beta + rnorm(1, 0, sigma)
+    beta <- beta + rnorm(1, 0, sig_beta*sqrt(dt[i]))
+}
+
+plot(t, logitp, type = "l")
+points(t_obs, logitp[t %in% t_obs])
+
+# Calculate p and draw a sample at the time of observation
+p <- exp(logitp) / (1 + exp(logitp))
+
+occu <- rbinom(length(t_obs), 1, p[t %in% t_obs])
+
+plot(t, p, type = "l", ylim = c(0, 1))
+points(t_obs, occu)
+
+
+# Create a time series of detections --------------------------------
+
+# Probability of observing given presence
+pcond <- 0.5
+
+occu_obs <- rbinom(length(occu), 1, ifelse(occu == 1, 0.5, 0))
+
+plot(t, p, type = "l", ylim = c(0, 1))
+points(t_obs, occu)
+points(t_obs, occu_obs, col = "red")
+
+# Create animated plot ----------------------------------------------------
+
+pres <- rep(NA, length = length(p))
+obs <- rep(NA, length = length(p))
+pres[t %in% t_obs] <- occu
+obs[t %in% t_obs] <- occu_obs
+
+# jitter obs a little
+obs <- ifelse(obs == 1, 0.95, 0.05)
+
+df <- data.frame(t = t, dt = dt, p = p, pres = pres, obs = obs)
+
+df %>%
+    ggplot() +
+    geom_line(aes(x = t, y = p), col = "blue") +
+    geom_point(aes(x = t, y = pres, group = seq_along(t)), size = 3, col = "blue") +
+    geom_point(aes(x = t, y = obs, group = seq_along(t)), size = 3) +
+    xlab("Time") + ylab("Probability of occurrence") +
+    theme_bw() +
+    theme(axis.title = element_text(size = 15),
+          axis.text = element_text(size = 13)) +
+    transition_reveal(t)
+
+anim_save("comms/output/occu_obs_anim.gif")
