d328f4e4d85985d4a91b481d4843dd09af1db587
patchcervan <francerper@gmail.com>
2021-07-14 15:16:03 +0200

    Changed arguments names to avoid conflicts in nested function calls

--- R/getRegionPentads.R
+++ R/getRegionPentads.R
@@ -1,10 +1,10 @@
 #' Get SABAP2 pentads for a certain region
 #'
-#' @param country A character string with the name of the country the region of
+#' @param .country A character string with the name of the country the region of
 #' interest is at. Capitalize first letters of each word.
-#' @param province A character string. If provided it subsets the region of
+#' @param .province A character string. If provided it subsets the region of
 #' interest to a specific province. Capitalize first letters of each word.
-#' @param path A directory where administrative boundaries layer should be found
+#' @param .path A directory where administrative boundaries layer should be found
 #' or stored at.
 #'
 #' @return A simple feature object with those pentads that are contained in the
@@ -17,25 +17,25 @@
 #'
 #' @examples
 #' # getRegionPentads(country = "South Africa", province = "North West")
-getRegionPentads <- function(country, province = NULL, path = NULL){
+getRegionPentads <- function(.country, .province = NULL, .path = NULL){
 
     pentads <- pentads
 
     # Download/load geographic data
-    if(is.null(path)){
-        region <- raster::getData("GADM", download = TRUE, country = country,
+    if(is.null(.path)){
+        region <- raster::getData("GADM", download = TRUE, country = .country,
                                   level = 1, path = tempdir()) %>%
             sf::st_as_sf()
     } else {
-        region <- raster::getData("GADM", download = TRUE, country = country,
-                                  level = 1, path = path) %>%
+        region <- raster::getData("GADM", download = TRUE, country = .country,
+                                  level = 1, path = .path) %>%
             sf::st_as_sf()
     }
 
     # Cut to study area
-    if(!is.null(province)){
+    if(!is.null(.province)){
         region <- region %>%
-            dplyr::filter(NAME_1 == province)
+            dplyr::filter(NAME_1 == .province)
     }
 
     # Find pentads that touch the region
