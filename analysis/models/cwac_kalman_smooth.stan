57093712c9c1a13e35fdf5b324974cd3c9fc0e35
patchcervan <francerper@gmail.com>
2021-08-06 18:38:49 +0200

    Fixed temporal indexing and intial values

--- analysis/models/cwac_kalman_smooth.stan
+++ analysis/models/cwac_kalman_smooth.stan
@@ -14,7 +14,7 @@ data {
   // observation matrix
   matrix[D, M] H;
   // time intervals
-  vector[N] dt;
+  vector[N-1] dt;
   // Priors for rate parameters
   real<lower = 0> psig_alpha;
   real<lower = 0> psig_beta;
@@ -30,9 +30,6 @@ parameters {
   real mu0;
   real beta0;
   real lambda0;
-  real<lower = 0> sig_alpha0;
-  real<lower = 0> sig_beta0;
-  real<lower = 0> sig_lambda0;
 
   // Standard deviation of rate parameters
   real<lower = 0> sig_alpha;
@@ -49,41 +46,49 @@ transformed parameters {
   // observation errors
   vector[N] R;
   // Predicted states
-  vector[M] a[N + 1];
+  vector[M] a[N];
   // Predicted covariance
-  matrix[M, M] P[N + 1];
+  matrix[M, M] P[N];
   // Estimated states
   vector[M] ahat[N];
   // Estimated covariances
   matrix[M, M] Phat[N];
   // Transition matrices
-  matrix[M,M] At[N];
-  matrix[M,M] Qt[N];
+  matrix[M,M] At[N-1];
+  matrix[M,M] Qt[N-1];
   // Likelihood
   vector[N] llik_obs = rep_vector(0, N);
 
+  // variances
+  real<lower = 0> var_alpha = pow(sig_alpha,2);
+  real<lower = 0> var_beta = pow(sig_beta,2);
+  real<lower = 0> var_lambda = pow(sig_lambda,2);
+  real<lower = 0> var_s = pow(sig_s,2);
+  real<lower = 0> var_w = pow(sig_w,2);
+  real<lower = 0> var_o = pow(sig_o,2);
+
   // 1st observation
   vector[3] aa1 = [mu0, beta0, lambda0]';
-  vector[3] pp1 = [sig_alpha0, sig_beta0, sig_lambda0]';
+  vector[3] pp1 = [1, 1, 1]';
   a[1] = aa1;
   P[1] = diag_matrix(pp1);
+  R[1] = var_s * summer[1] + var_w * winter[1] + var_o - var_o * (summer[1] + winter[1]);
+  llik_obs[1] += normal_lpdf(y[1]| mu0+beta0, sqrt(R[1]));
 
   // precompute all transition matrices
-  for(i in 1:N){
+  for(i in 1:(N-1)){
     // Transition matrices
     At[i] = [ [1, summer[i], winter[i]], [0, 1, 0], [0, 0, 1] ];
     // Covariance matrices
-    Qt[i] = [ [sig_alpha, 0, 0], [0, sig_beta, 0], [0, 0, sig_lambda] ] * sqrt(dt[i]);
+    Qt[i] = [ [var_alpha, 0, 0], [0, var_beta, 0], [0, 0, var_lambda] ] * dt[i];
     // observation error
-    R[i] = sig_s * summer[i] + sig_w * winter[i] + sig_o - sig_o * (summer[i] + winter[i]);
+    R[i+1] = var_s * summer[i+1] + var_w * winter[i+1] + var_o - var_o * (summer[i+1] + winter[i+1]);
   }
 
   // iterative estimation
-  for (i in 1:N) {
-    matrix[M,M] Bt;
+  for (i in 1:(N-1)) {
     vector[D] v;
     matrix[D, D] S;
-    // vector[D] Sdiag;
     matrix[D, D] Sinv;
     matrix[M, D] K;
     matrix[M, M] L;
@@ -94,8 +99,8 @@ transformed parameters {
     Phat[i] = At[i] * P[i] * At[i]' + Qt[i];
 
     // Update
-    v = y[i] - H * ahat[i];
-    S = H * Phat[i] * H' + R[i];
+    v = y[i+1] - H * ahat[i];
+    S = H * Phat[i] * H' + R[i+1];
     K = mdivide_right_spd(Phat[i] * H', S);
 
     a[i + 1] = ahat[i] + K * v;
@@ -112,12 +117,9 @@ transformed parameters {
 
 model {
   // Priors for initial states
-  mu0 ~ normal(0, 5);
+  mu0 ~ normal(5, 5);
   beta0 ~ normal(0, 5);
   lambda0 ~ normal(0, 5);
-  sig_alpha0 ~ cauchy(0, 5);
-  sig_beta0 ~ cauchy(0, 5);
-  sig_lambda0 ~ cauchy(0, 5);
 
   // Priors for standard deviation of rate parameters
   sig_alpha ~ cauchy(0, psig_alpha);
--- analysis/scripts/misc/ct_ssm_sims.R
+++ analysis/scripts/misc/ct_ssm_sims.R
@@ -16,10 +16,10 @@ N <- 100
 x0 <- 10
 
 # Initial change due to summer (this should be estimated)
-beta0 <- 0
+beta0 <- 2
 
 # Initial change due to winter (this should be estimated)
-lambda0 <- 0
+lambda0 <- -1
 
 # Vector of summer/winter/other indicators
 season <- rep(c("s", "w", "o"), length.out = N)
@@ -27,7 +27,7 @@ summer <- ifelse(season == "s", 1, 0)
 winter <- ifelse(season == "w", 1, 0)
 
 # Vector of time increments
-dt <- rnbinom(N-1, size = 5, mu = 2)
+dt <- rnbinom(N-1, size = 5, mu = 2)+1
 hist(dt)
 
 # Define process standard deviation parameters
@@ -36,10 +36,9 @@ sig_beta <- 3
 sig_lambda <- 0.5
 
 # Define observation error standard deviation parameters
-sig_s <- 1.5
-sig_w <- 0.5
-sig_o <- 1
-
+sig_s <- 2
+sig_w <- 6
+sig_o <- 10
 
 # Simulate process --------------------------------------------------------
 
@@ -47,18 +46,18 @@ sig_o <- 1
 X <- matrix(NA, nrow = 3, ncol = N)
 
 # Define initial state vector
-X[,1] <- matrix(c(x0, beta0, lambda0))
+X[,1] <- matrix(c(x0+beta0, beta0, lambda0)) # Plus beta0 just because the series starts with summer
 
 # Iterate through steps
 for(t in seq_len(N-1)){
 
-    # transition matrix
-    theta <- rbind(c(1, summer[t], winter[t]),
-                   c(0, 1, 0),
-                   c(0, 0, 1))
+  # transition matrix
+  theta <- rbind(c(1, summer[t], winter[t]),
+                 c(0, 1, 0),
+                 c(0, 0, 1))
 
-    # update process
-    X[,t+1] = theta %*% X[,t,drop=FALSE] + matrix(rnorm(3, 0, c(sig_alpha, sig_beta, sig_lambda)*sqrt(dt[t])))
+  # update process
+  X[,t+1] = theta %*% X[,t,drop=FALSE] + matrix(rnorm(3, 0, c(sig_alpha, sig_beta, sig_lambda)*sqrt(dt[t])))
 }
 
 plot(X[1,])
@@ -70,6 +69,112 @@ plot(X[1,], type = "l")
 plot(X[2,], type = "l")
 plot(X[3,], type = "l")
 par(op)
+
+# Include observation error
+
+sigma <- (sig_s*summer + sig_w*winter + sig_o - sig_o*(summer + winter))
+
+y <- X[1,] + rnorm(N, 0, sigma)
+
+plot(y)
+
+plot(X[1,1:10], type = "l")
+lines(y[1:10], col = "red")
+
+
+
+# Fit model to recover parameters -----------------------------------------
+
+# Bundle data
+data.bundle <- list(  # number of observations
+    N = ncol(X),
+    # number of observed states
+    D = 1,
+    # number of states
+    M = nrow(X),
+    # observed data
+    y = matrix(y),
+    # season indicators
+    summer = summer,
+    winter = winter,
+    # observation matrix
+    H = matrix(c(1, 0, 0), nrow = 1),
+    # time increments
+    dt = dt,
+    # Priors for rate parameters
+    psig_alpha = 2,
+    psig_beta = 2,
+    psig_lambda = 2,
+    # Priors for observation error
+    psig_s = 5,
+    psig_w = 5,
+    psig_o = 5
+)
+
+# Define parameters to plot
+paramToPlot <- c("sig_alpha", "sig_beta", "sig_lambda",
+                 "sig_s", "sig_w", "sig_o", "mu0", "beta0", "lambda0")
+
+param <- c(paramToPlot)
+
+# Compile model
+stan_mod <- stan_model(file = "analysis/models/cwac_kalman_smooth.stan")
+
+# Define initial values
+init = function() list(
+  mu0 = rexp(1, 1),
+  beta0  = rnorm(1, 3, 3),
+  lambda0  = rnorm(1, -3, 3),
+
+  sig_alpha = rexp(1, 1),
+  sig_beta = rexp(1, 1),
+  sig_lambda = rexp(1, 1),
+
+  sig_s = rexp(1, 1),
+  sig_w = rexp(1, 1),
+  sig_o  = rexp(1, 1)
+)
+
+# gc()
+fit <- sampling(stan_mod,
+                data = data.bundle,
+                pars = param, init = init,
+                iter = 2000, chains = 4, warmup = 1000, thin = 1,
+                cores = getOption("mc.cores", 4),
+                control = list(adapt_delta = 0.95),
+                refresh = max(2000/50,1),
+                save_warmup = FALSE)
+
+stan_trace(fit, pars = paramToPlot)
+print(fit, pars = paramToPlot, probs = c(0.025, 0.5, 0.975),
+      digits_summary = 3, use_cache = F)
+
+fit$sig_lambda
+
+
+
+# Simulate no-latent process ----------------------------------------------
+
+# Response
+y <- vector(length = N)
+y[1] <- y0
+
+theta <- matrix(c(beta0, lambda0))
+
+for(t in 1:(N-1)){
+
+  sigma <- (sig_s*summer[t+1] + sig_w*winter[t+1] + sig_o - sig_o*(summer[t+1] + winter[t+1]))*sqrt(dt[t])
+
+  Ht <- cbind(c(summer[t]),
+              c(winter[t]))
+
+  y[t+1] <- y[t] + Ht %*% theta + rnorm(1, 0, sigma)
+
+}
+
+plot(y)
+plot(y[1:10])
+
 
 
 # Simulate multiple trajectories ------------------------------------------
@@ -103,79 +208,3 @@ par(op)
 # for(j in seq_len(J-1)){
 #     lines(X[1,,j+1], col = j+1)
 # }
-
-
-
-# Fit model to recover parameters -----------------------------------------
-
-
-# Bundle data
-data.bundle <- list(  # number of observations
-    N = ncol(X),
-    # number of observed states
-    D = 1,
-    # number of states
-    M = nrow(X),
-    # observed data
-    y = t(X[1,,drop = F]),
-    # season indicators
-    summer = summer,
-    winter = winter,
-    # observation matrix
-    H = matrix(c(1, 0, 0), nrow = 1),
-    # time increments
-    dt = c(0.1, dt),
-    # Priors for rate parameters
-    psig_alpha = 3,
-    psig_beta = 3,
-    psig_lambda = 3,
-    # Priors for observation error
-    psig_s = 2,
-    psig_w = 2,
-    psig_o = 2
-)
-
-# Define parameters to plot
-paramToPlot <- c("sig_alpha", "sig_beta", "sig_lambda",
-                 "sig_s", "sig_w", "sig_o")
-
-param <- c(paramToPlot)
-
-# Compile model
-stan_mod <- stan_model(file = "analysis/models/cwac_kalman_smooth.stan")
-
-# Define initial values
-init = function() list(
-  mu0 = rnorm(1, 0, 1),
-  beta0  = rnorm(1, 0, 1),
-  lambda0  = rnorm(1, 0, 1),
-  sig_alpha0 = rexp(1, 1),
-  sig_beta0 = rexp(1, 1),
-  sig_lambda0 = rexp(1, 1),
-
-  sig_alpha = rexp(1, 1),
-  sig_beta = rexp(1, 1),
-  sig_lambda = rexp(1, 1),
-
-  sig_s = rexp(1, 1),
-  sig_w = rexp(1, 1),
-  sig_o  = rexp(1, 1)
-)
-
-# gc()
-start_time <- Sys.time()
-fit <- sampling(stan_mod,
-                data = data.bundle,
-                pars = param, init = init,
-                iter = 2000, chains = 4, warmup = 1000, thin = 1,
-                cores = getOption("mc.cores", 4),
-                control = list(adapt_delta = 0.85),
-                refresh = max(2000/50,1),
-                save_warmup = FALSE)
-end_time <- Sys.time()
-end_time - start_time
-
-stan_trace(fit, pars = paramToPlot)
-print(fit, pars = paramToPlot, probs = c(0.025, 0.5, 0.975),
-      digits_summary = 3, use_cache = F)
-
