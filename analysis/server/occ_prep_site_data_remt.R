76765f7424c58492d866f15c65c9e1014d95358a
patchcervan <francerper@gmail.com>
2021-09-20 18:48:10 +0200

    Fixed paths

--- analysis/server/occ_prep_site_data_remt.R
+++ analysis/server/occ_prep_site_data_remt.R
@@ -4,7 +4,7 @@
 
 library(BIRDIE)
 
-birdie_dir <- "/drv_birdie/Working/git/"
+birdie_dir <- "/home/birdie/analysis/"
 
 rm(list = ls())
 
@@ -16,10 +16,10 @@ sites <- prepOccSiteData(#region = "South Africa",
                          sites = sites,
                          years = 2008:2019,
                          clim_covts = c("prcp", "tmax", "tmin", "aet", "pet"),
-                         covts_dir = paste0(birdie_dir, "analysis/downloads/"),
+                         covts_dir = paste0(birdie_dir, "downloads/"),
                          file_fix = c("terraClim_", "_03_19"),
-                         savedir = paste0(birdie_dir, "analysis/data/pentads_sa.rds"))
+                         savedir = paste0(birdie_dir, "data/pentads_sa.rds"))
 
 future::plan("sequential")
 
-saveRDS(sites, paste0(birdie_dir, "analysis/data/site_dat_sa_wcovts_16_19.rds"))
+saveRDS(sites, paste0(birdie_dir, "data/site_dat_sa_wcovts_16_19.rds"))
