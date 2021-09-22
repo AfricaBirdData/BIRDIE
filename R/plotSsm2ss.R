9ed0f72db41232c9c1de47be7253b78e9ea8f99c
patchcervan <francerper@gmail.com>
2021-09-22 11:50:16 +0200

    plotSsm2ss can plot on original scale

--- R/plotSsm2ss.R
+++ R/plotSsm2ss.R
@@ -3,7 +3,8 @@
 #' @param fit A JAGS state-space model fitted to CWAC data
 #' @param ssm_counts A data frame with the count data use to fit the
 #' state-space model
-#' @param dyn Whether the fitted long-term trend is fixed or dynamic (NOT USED AT PRESENT).
+#' @param linear If TRUE (default) abundance estimates and data are
+#' transformed back to its orginal scale.
 #' @param plot_options A list with two elements: colors - the colours of the
 #' points that will appear in the plot (two values), and pers_theme  - A
 #' personalized ggplot theme.
@@ -18,8 +19,8 @@
 #' counts <- barberspan
 #' ssmcounts <- prepSsmData(counts, species = NULL)
 #' fit <- fitCwacSsm(ssmcounts, param = c("beta", "lambda", "sig.zeta", "sig.w", "sig.eps", "sig.alpha", "sig.e", "mu_t", "mu_wt"))
-#' plotSsm2ss(fit = fit, ssm_counts = ssmcounts, dyn = TRUE)
-plotSsm2ss <- function(fit, ssm_counts, dyn = FALSE,
+#' plotSsm2ss(fit = fit, ssm_counts = ssmcounts)
+plotSsm2ss <- function(fit, ssm_counts, linear = TRUE,
                        plot_options = list(colors = NULL, pers_theme = NULL)){
 
     if(is.null(plot_options$colors)){
@@ -35,84 +36,94 @@ plotSsm2ss <- function(fit, ssm_counts, dyn = FALSE,
                            count = c(log(ssm_counts[ssm_counts$season_id==1, "count", drop = T] + 0.1),
                                      log(ssm_counts[ssm_counts$season_id==2, "count", drop = T] + 0.1)))
 
-    # Plot separated by season
-    stt_plot <- post_stt %>%
-        tidyr::pivot_longer(cols = c(mu_est, mu_lb, mu_ub),
-                     names_to = "quantile") %>%
-        ggplot2::ggplot() +
-        ggplot2::geom_path(aes(x = year, y = value, linetype = quantile)) +
-        ggplot2::geom_point(aes(x = year, y = count, col = factor(season)), show.legend = FALSE) +
-        # ggplot2::scale_colour_discrete(name = "Season", labels = c("Summer", "Winter")) +
-        ggplot2::scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
-                                       #labels = c("Mean", "2.5%", "97.5%")) +
-        ggplot2::scale_colour_manual(values = plot_options$colors) +
-        ggplot2::facet_wrap("season", nrow = 2,
-                            labeller = ggplot2::labeller(season = c("1" = "Summer", "2" = "Winter"))) +
-        ggplot2::xlab("Year") + ggplot2::ylab("log-abundance") +
-        plot_options$pers_theme
+    if(linear){
+        post_stt <- post_stt %>%
+            dplyr::mutate(dplyr::across(.cols = -c(year, season),
+                                        .fns = ~exp(.x) - 0.1))
+        abund_label <- "Abundance"
 
-    # Create a plot for the trend
-    if(dyn == FALSE){
-
-        # Create a data frame with the posterior trend
-        post_trd <- data.frame(trd_est = fit$mean$beta,
-                               trd_lb = fit$q2.5$beta,
-                               trd_ub = fit$q97.5$beta,
-                               prop_est = fit$mean$lambda,
-                               prop_lb = fit$q2.5$lambda,
-                               prop_ub = fit$q97.5$lambda)
-
-        trd_plot <- data.frame(beta = fit$sims.list$beta) %>%
-            ggplot2::ggplot() +
-            ggplot2::geom_density(aes(x = beta), fill = "yellow") +
-            ggplot2::geom_pointrange(data = post_trd, aes(x = trd_est, xmin = trd_lb, xmax = trd_ub, y = 0)) +
-            ggplot2::scale_colour_manual(values = plot_options$colors) +
-            ggplot2::xlab("Trend (log growth-rate)") +
-            plot_options$pers_theme
-
-        prop_plot <- data.frame(lambda = fit$sims.list$lambda) %>%
-            ggplot2::ggplot() +
-            ggplot2::geom_density(aes(x = lambda), fill = "yellow") +
-            ggplot2::geom_pointrange(data = post_trd, aes(x = trd_est, xmin = trd_lb, xmax = trd_ub, y = 0)) +
-            ggplot2::scale_colour_manual(values = plot_options$colors) +
-            ggplot2::xlab("Log propotion summer/winter") +
-            plot_options$pers_theme
+        # Cut axis when values are larger than 10 times the max count
+        ylims <- c(0, max(post_stt$count, na.rm = T) * 10)
 
     } else {
 
-        # Create a data frame with the posterior trend
-        post_trd <- data.frame(beta_est = fit$mean$beta,
-                               beta_lb = fit$q2.5$beta,
-                               beta_ub = fit$q97.5$beta,
-                               prop_est = fit$mean$lambda,
-                               prop_lb = fit$q2.5$lambda,
-                               prop_ub = fit$q97.5$lambda,
-                               year = unique(ssm_counts$year))
+        abund_label <- "log abundance"
+        ylims <- c(NA, NA)
 
-        # Plot trend
-        trd_plot <- post_trd %>%
-            dplyr::select(-dplyr::starts_with("prop")) %>%
-            tidyr::pivot_longer(cols = -year,
-                                names_to = "quantile") %>%
-            ggplot2::ggplot() +
-            ggplot2::geom_path(aes(x = year, y = value, linetype = quantile)) +
-            ggplot2::scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
-            ggplot2::scale_colour_manual(values = plot_options$colors) +
-            ggplot2::xlab("Year") + ggplot2::ylab("log growth-rate") +
-            plot_options$pers_theme
-
-        # Plot proportion summer/winter
-        prop_plot <- post_trd %>%
-            dplyr::select(-dplyr::starts_with("beta")) %>%
-            tidyr::pivot_longer(cols = -year,
-                                names_to = "quantile") %>%
-            ggplot2::ggplot() +
-            ggplot2::geom_path(aes(x = year, y = value, linetype = quantile)) +
-            ggplot2::scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
-            ggplot2::scale_colour_manual(values = plot_options$colors) +
-            ggplot2::xlab("Year") + ggplot2::ylab("Log proportion") +
-            plot_options$pers_theme
     }
+
+
+    # Abundance by season -----------------------------------------------------
+
+    stt_plot <- post_stt %>%
+        tidyr::pivot_longer(cols = c(mu_est, mu_lb, mu_ub),
+                            names_to = "quantile") %>%
+        ggplot2::ggplot() +
+        ggplot2::geom_path(aes(x = year, y = value, linetype = quantile)) +
+        ggplot2::geom_point(aes(x = year, y = count, col = factor(season)), show.legend = FALSE) +
+        ggplot2::scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
+        ggplot2::scale_colour_manual(values = plot_options$colors) +
+        ggplot2::coord_cartesian(ylim = ylims) +
+        ggplot2::facet_wrap("season", nrow = 2,
+                            labeller = ggplot2::labeller(season = c("1" = "Summer", "2" = "Winter"))) +
+        ggplot2::xlab("Year") + ggplot2::ylab(abund_label) +
+        plot_options$pers_theme
+
+
+    # Trend plot --------------------------------------------------------------
+
+    # Create a data frame with the posterior trend
+    post_trd <- data.frame(beta_est = fit$mean$beta,
+                           beta_lb = fit$q2.5$beta,
+                           beta_ub = fit$q97.5$beta,
+                           prop_est = fit$mean$lambda,
+                           prop_lb = fit$q2.5$lambda,
+                           prop_ub = fit$q97.5$lambda,
+                           year = unique(ssm_counts$year))
+
+    if(linear){
+        post_trd <- post_trd %>%
+            dplyr::mutate(dplyr::across(.cols = -year,
+                                        .fns = ~exp(.x)))
+        rate_label <- "Growth rate"
+        ratio_label <- "W/S ratio"
+
+    } else {
+
+        rate_label <- "log growth rate"
+        ratio_label <- "log W/S ratio"
+
+    }
+
+    # Plot trend
+    trd_plot <- post_trd %>%
+        dplyr::select(-dplyr::starts_with("prop")) %>%
+        tidyr::pivot_longer(cols = -year,
+                            names_to = "quantile") %>%
+        ggplot2::ggplot() +
+        ggplot2::geom_path(aes(x = year, y = value, linetype = quantile)) +
+        ggplot2::scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
+        ggplot2::scale_colour_manual(values = plot_options$colors) +
+        ggplot2::xlab("Year") + ggplot2::ylab(rate_label) +
+        plot_options$pers_theme
+
+
+    # Winter/summer ratio -----------------------------------------------------
+
+    # Plot proportion summer/winter
+    prop_plot <- post_trd %>%
+        dplyr::select(-dplyr::starts_with("beta")) %>%
+        tidyr::pivot_longer(cols = -year,
+                            names_to = "quantile") %>%
+        ggplot2::ggplot() +
+        ggplot2::geom_path(aes(x = year, y = value, linetype = quantile)) +
+        ggplot2::scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
+        ggplot2::scale_colour_manual(values = plot_options$colors) +
+        ggplot2::xlab("Year") + ggplot2::ylab(ratio_label) +
+        plot_options$pers_theme
+
+
+    # Save --------------------------------------------------------------------
 
     # Title
     plottitle <- ifelse(unique(ssm_counts$spp) == "multi",
