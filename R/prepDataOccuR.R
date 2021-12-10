015aacca35fcbd9668d8b3b36780e77aa31a011f
patchcervan <francerper@gmail.com>
2021-12-10 13:33:25 +0200

    Removed old functions

--- R/prepDataOccuR.R
+++ R/prepDataOccuR.R
@@ -1,9 +1,7 @@
 #' Prepare data for fitting a model with occuR
 #'
-#' @param site_data A dataframe with occupancy site data produced by
-#' \link{prepOccSiteData}.
-#' @param visit_data A dataframe with occupancy visit data produced by
-#' \link{prepOccVisitData}.
+#' @param site_data A dataframe with occupancy site data.
+#' @param visit_data A dataframe with occupancy visit data.
 #'
 #' @return A list containing two data frames: one with site data and one with
 #' visit data ready to use with the occuR package
@@ -19,41 +17,51 @@ prepDataOccuR <- function(site_data, visit_data){
 
     sites <- unique(visit_data$Pentad)
 
-    sitedata <- site_data %>%
-        sf::st_drop_geometry()  %>%
+    # Keep those sites that appear in visits data and drop geometry
+    site_data <- site_data %>%
+        sf::st_drop_geometry() %>%
+        dplyr::filter(Name %in% sites) %>%
         dplyr::group_by(Name) %>%
         dplyr::mutate(site = dplyr::cur_group_id()) %>%
-        tidyr::pivot_longer(cols = -c(Name, lon, lat, site, watocc_ever)) %>%
+        dplyr::ungroup()
+
+    # Separate covariates and years
+    site_data <- site_data %>%
+        tidyr::pivot_longer(cols = -c(Name, lon, lat, site, watocc_ever, dist_coast)) %>%
         tidyr::separate(name, into = c("covt", "year"), sep = "_") %>%
         tidyr::pivot_wider(names_from = covt, values_from = value) %>%
         dplyr::mutate(year = as.numeric(year)) %>%
-        dplyr::left_join(visits, by = c("Name" = "Pentad", "year" = "year")) %>%
-        dplyr::filter(keep == 1) %>%
-        dplyr::select(-keep) %>%
         dplyr::group_by(year) %>%
         dplyr::mutate(occasion = cur_group_id()) %>%
-        dplyr::ungroup() %>%
-        data.table::as.data.table()
+        dplyr::ungroup()
 
-    visitdata <- visit_data %>%
-        dplyr::left_join(sitedata %>%
+    if(any(is.na(site_data$year))){
+        warning("There might be covariates that don't change over time other than watocc_ever and dist_coast.")
+    }
+
+    # Transfer site and occasion indicators from site data
+    visit_data <- visit_data %>%
+        dplyr::left_join(site_data %>%
                              dplyr::select(Name, site) %>%
-                             distinct(),
+                             dplyr::distinct(),
                          by = c("Pentad" = "Name")) %>%
-        dplyr::left_join(sitedata %>%
+        dplyr::left_join(site_data %>%
                              dplyr::select(year, occasion) %>%
-                             distinct(),
-                         by = "year") %>%
+                             dplyr::distinct(),
+                         by = "year")
+
+    # Add visit indicator
+    visit_data <- visit_data %>%
         dplyr::group_by(site, occasion) %>%
         dplyr::mutate(visit = row_number()) %>%
         dplyr::ungroup() %>%
-        dplyr::select(-c(CardNo, Date)) %>%
-        data.table::as.data.table()
+        dplyr::select(-c(CardNo, Date))
 
-    if(any(is.na(visitdata$site))){
+    if(any(is.na(visit_data$site))){
         warning("Sites differ between site data and visit data!")
     }
 
-    return(list(site = sitedata, visit = visitdata))
+    return(list(site = data.table::as.data.table(site_data),
+                visit = data.table::as.data.table(visit_data)))
 
 }
