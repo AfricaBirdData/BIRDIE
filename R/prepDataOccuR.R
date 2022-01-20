4171f60e2845cc2addb2c9e3886ef34dc4715c28
patchcervan <francerper@gmail.com>
2022-01-20 15:18:27 +0200

    Included all data preparation in prepDataOccuR

--- R/prepDataOccuR.R
+++ R/prepDataOccuR.R
@@ -1,17 +1,77 @@
 #' Prepare data for fitting a model with occuR
 #'
+#' @description This function takes generic occupancy visit and site data
+#' without species observations, adds detection data for a species in a given
+#' year and formats the output to match occuR standards.
+#' @param spp_code SAFRING_No of the species of interest as extracted from ABAP.
+#' Ignored if download set to FALSE.
+#' @param years A numeric vector with elements corresponding to the years we
+#' want data for. Ignored if download set to FALSE.
 #' @param site_data A dataframe with occupancy site data.
 #' @param visit_data A dataframe with occupancy visit data.
-#' @param scaling Either NULL, in which case no scaling is performed or a list
-#' of two named vectors: "site" and "visit" indicating the variables to scale
-#' in the site and visit dataframes.
+#' @param download Indicates whether ABAP data must be downloaded for the species
+#' and years indicated by 'spp_code' and 'years'. If TRUE, data will be downloaded
+#' from ABAP once per session and cached in a temp file. After this the cached
+#' file will be used, unless download is set to "force", in which case data will
+#' be downloaded regardless of the cached file. If FALSE data will not be
+#' downloaded or retrieved from cache.
 #'
 #' @return A list containing two data frames: one with site data and one with
 #' visit data ready to use with the occuR package
 #' @export
 #'
 #' @examples
-prepDataOccuR <- function(site_data, visit_data, scaling = NULL){
+prepDataOccuR <- function(spp_code = NULL, years = NULL,
+                          site_data, visit_data,
+                          download = FALSE){
+
+    if(isFALSE(download)){
+        if(!is.null(spp_code) | !is.null(years)){
+            warning("spp_code and years are ignored if download is FALSE")
+        }
+    }
+
+    if(!isFALSE(download)){
+        if(!is.null(spp_code) & !is.null(years)){
+
+            # Cache file name
+            cachefile <- tempfile(pattern = paste(c("abap", spp_code, years), collapse = "_"),
+                                  fileext = ".rds")
+
+            if(file.exists(cachefile) & download != "force"){
+                print("Using cached file")
+                sp_detect <- readRDS(cachefile)
+
+            } else {
+
+                # Download species detection
+                print("Downloading from SABAP")
+
+                sp_detect <- ABAP::getAbapData(.spp_code = spp_code,
+                                               .region_type = "country",
+                                               .region = "South Africa",
+                                               .years = years)
+
+                # Save to cache
+                saveRDS(sp_detect, cachefile)
+            }
+
+            # Add detections to visit data
+            visit_data <- visit_data %>%
+                dplyr::left_join(sp_detect %>%
+                                     dplyr::select(CardNo, obs = Spp) %>%
+                                     dplyr::mutate(obs = if_else(obs == "-", 0, 1)),
+                                 by = "CardNo")
+        } else {
+            stop("spp_code and years must be provided if download is TRUE")
+        }
+    }
+
+    # Long format for site variables and years
+    site_data <- site_data %>%
+        sf::st_drop_geometry() %>%
+        BIRDIE::gatherYearFromVars(vars = names(.)[-c(1:5)], sep = "_") %>% #  # HARD CODED. Check that 1:5 are the variables that don't change over time
+        dplyr::mutate(tdiff = tmmx - tmmn)
 
     # Visited sites
     visits <- visit_data %>%
@@ -65,7 +125,13 @@ prepDataOccuR <- function(site_data, visit_data, scaling = NULL){
     # Make data.tables
     site_data <- data.table::as.data.table(site_data)
     visit_data <- data.table::as.data.table(visit_data)
-    # Scale?
+
+
+    # Scaling -----------------------------------------------------------------
+
+    scaling <- list(visit = NULL,
+                   site = c("dist_coast", "prcp", "tdiff", "ndvi", "watext", "watrec")) # HARD CODED
+
     if(!is.null(scaling)){
 
         if(!is.null(scaling$visit)){
@@ -85,6 +151,13 @@ prepDataOccuR <- function(site_data, visit_data, scaling = NULL){
         }
 
     }
+
+    # Remove data from missing pentads? (THIS SHOULDN'T HAPPEN WHEN PENTADS ARE DOWNLOADED FROM THE API)
+    visit_data <- visit_data %>%
+        dplyr::filter(!is.na(site))
+
+    site_data <- site_data %>%
+        dplyr::filter(site %in% unique(visit_data$site))
 
     return(list(site = site_data,
                 visit = visit_data))
