c9b0dc5bceb160f3c7bbff7ea43c4fdd57f6a218
patchcervan <francerper@gmail.com>
2022-02-02 13:46:16 +0200

    Created pipeline functions to run the pipeline and prepare data

---
+++ R/ppl_create_site_visit.R
@@ -0,0 +1,45 @@
+#' Create site and visit occupancy files
+#'
+#' @inheritParams ppl_run_pipe_distr
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ppl_create_site_visit <- function(sp_code, year, config, ...){
+
+    varargs <- list(...)
+
+    # Data file name
+    if(year < 2020){
+        datafile <- file.path(config$data_dir, "site_dat_sa_gee_08_19.rds")
+    } else {
+        yy <- substring(as.character(config$year), 3, 4)
+        ff <- paste0("site_dat_sa_gee_", yy, ".rds")
+        datafile <- file.path(config$data_dir, ff)
+    }
+
+    # Load data and subset years
+    sitedata <- readRDS(datafile) %>%
+        sf::st_drop_geometry() %>%
+        dplyr::select(Pentad = Name, lon, lat, watocc_ever, dist_coast, ends_with(match = as.character(config$years))) %>%
+        tidyr::drop_na()   # I'M REMOVING SITES WITH NA DATA! MAKE SURE THIS MAKES SENSE
+
+    visitdata <- readRDS(file.path(config$data_dir, "visit_dat_sa_gee_08_19.rds")) %>%
+        dplyr::filter(year %in% config$years)
+
+
+    # Format to occuR ---------------------------------------------------------
+
+    occuRdata <- BIRDIE::prepDataOccuR(spp_code = sp_code,
+                                       years = config$years,
+                                       site_data = sitedata,
+                                       visit_data = visitdata,
+                                       config = config,
+                                       download = varargs$download_from_abap,
+                                       save = varargs$save_occu_data,
+                                       overwrite = varargs$overwrite_occu_data)
+
+    return(occuRdata)
+
+}
---
+++ R/ppl_run_pipe_distr.R
@@ -0,0 +1,62 @@
+#' Run distribution indicators pipeline
+#'
+#' @param sp_code SAFRING code of the species to run the pipeline for
+#' @param year Year to run to the pipeline for
+#' @param config A list with pipeline configuration parameters.
+#' See \link{configPreambOccuR}
+#' @param steps A character vector containing the steps of the pipeline to run.
+#' Defaults to all of them.
+#' @param ... Other arguments passed on to other functions
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ppl_run_pipe_distr <- function(sp_code, year, config,
+                               steps = c("data", "fit", "summ", "indtr"), ...){
+
+    if("data" %in% steps){
+        ppl_create_site_visit(sp_code, year, config, ...)
+    }
+
+    if("fit" %in% steps){
+        ppl_fit_occur_model(sp_code, year, ...)
+    }
+
+    if("summ" %in% steps){
+        ppl_summarize_occur(sp_code, year, ...)
+    }
+
+    if("indtr" %in% steps){
+        # Estimate area of occupancy (AOO) for the year or also previous years
+        # if year is earlier than 2010 (i.e. initial time window)
+        for(t in seq_along(config$years)){
+
+            if((t > config$dyear/2) | (config$year < (2009 + config$dyear/2))){
+                ppl_estimate_aoo(sp_code, year = config$years[t], ...)
+            }
+
+        }
+
+        # Estimate annual change in AOO and if year is greater than 2017,
+        # also estimate short term change in AOO (i.e. in previous 10 years)
+        if(year != 2017){
+            for(t in seq_along(config$years[-1])){
+
+                if(config$years[t+1] > 2008){
+                    ppl_estimate_daoo(sp_code, year = config$years[t+1], term = "annual", ...)
+                }
+
+            }
+
+            for(t in seq_along(config$years)){
+
+                if(config$years[t] > 2017){
+                    ppl_estimate_daoo(sp_code, year = config$years[t], term = "short", ...)
+                }
+
+            }
+        }
+    }
+
+}
