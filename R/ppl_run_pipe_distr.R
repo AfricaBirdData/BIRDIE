89e1fff03d795ccc889953ffc1ad3615456ccc5e
patchcervan <francerper@gmail.com>
2022-02-03 13:29:01 +0200

    Fixed conditions necessary for indicators to be computed

--- R/ppl_run_pipe_distr.R
+++ R/ppl_run_pipe_distr.R
@@ -34,7 +34,7 @@ ppl_run_pipe_distr <- function(sp_code, sp_name, year, config,
         # if year is earlier than 2010 (i.e. initial time window)
         for(t in seq_along(config$years)){
 
-            if((t > config$dyear/2) | (config$year < (2009 + config$dyear/2))){
+            if((t > config$dyear/2) | (config$year > (2009 + config$dyear/2))){
                 ppl_estimate_aoo(sp_code, year = config$years[t], config, ...)
             }
 
@@ -42,23 +42,22 @@ ppl_run_pipe_distr <- function(sp_code, sp_name, year, config,
 
         # Estimate annual change in AOO and if year is greater than 2017,
         # also estimate short term change in AOO (i.e. in previous 10 years)
-        if(year != 2017){
-            for(t in seq_along(config$years[-1])){
+        for(t in seq_along(config$years[-1])){
 
-                if(config$years[t+1] > 2008){
-                    ppl_estimate_daoo(sp_code, year = config$years[t+1], config, term = "annual", ...)
-                }
-
+            if(config$years[t+1] > 2008){
+                ppl_estimate_daoo(sp_code, year = config$years[t+1], config, term = "annual", ...)
             }
 
-            for(t in seq_along(config$years)){
-
-                if(config$years[t] > 2017){
-                    ppl_estimate_daoo(sp_code, year = config$years[t], config, term = "short", ...)
-                }
-
-            }
         }
+
+        for(t in seq_along(config$years)){
+
+            if(config$years[t] > 2017){
+                ppl_estimate_daoo(sp_code, year = config$years[t], config, term = "short", ...)
+            }
+
+        }
+
     }
 
 }
