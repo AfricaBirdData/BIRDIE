ff6c90e2430c591111cf97ce34cb73a08eab0365
patchcervan <francerper@gmail.com>
2022-03-07 16:04:47 +0200

    Added warning message for when there are no detections for a species

--- R/ppl_run_pipe_distr.R
+++ R/ppl_run_pipe_distr.R
@@ -27,6 +27,7 @@ ppl_run_pipe_distr <- function(sp_code, sp_name, year, config,
 
         # Stop if there are no detections
         if(fit_status == 1){
+            warning(paste("There are no detections for species", sp_code))
             return(1)
         }
     }
