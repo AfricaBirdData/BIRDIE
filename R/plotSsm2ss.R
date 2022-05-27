827e9a424e0b934f3dcece2c89dd043368e240d6
patchcervan <francerper@gmail.com>
2022-05-27 17:38:40 +0200

    Modified ssm summarizing functions for multi-site analysis

--- R/plotSsm2ss.R
+++ R/plotSsm2ss.R
@@ -37,6 +37,7 @@ plotSsm2ss <- function(fit, ssm_counts, linear = TRUE,
                            mu_ub = fit$q97.5$mu_t,
                            year = ssm_counts$year,
                            season = ssm_counts$Season,
+                           site = ssm_counts$LocationCode,
                            count = log(ssm_counts$count + 0.1)) %>%
         dplyr::filter(season != "O") %>%
         dplyr::mutate(season = ifelse(season == "S", 1, 2))
@@ -44,16 +45,16 @@ plotSsm2ss <- function(fit, ssm_counts, linear = TRUE,
     # There might be counts in more than one day per season and year, so
     # we plot the mean for the season
     post_stt <- post_stt %>%
-        dplyr::group_by(year, season) %>%
+        dplyr::group_by(year, season, site) %>%
         dplyr::summarize(count = mean(count),
                          mu_est = mean(mu_est),
                          mu_lb = mean(mu_lb),
                          mu_ub = mean(mu_ub)) %>%
-        ungroup()
+        dplyr::ungroup()
 
     if(linear){
         post_stt <- post_stt %>%
-            dplyr::mutate(dplyr::across(.cols = -c(year, season),
+            dplyr::mutate(dplyr::across(.cols = -c(year, season, site),
                                         .fns = ~exp(.x) - 0.1))
         abund_label <- "Abundance"
 
@@ -68,94 +69,108 @@ plotSsm2ss <- function(fit, ssm_counts, linear = TRUE,
     }
 
 
-    # Abundance by season -----------------------------------------------------
+    site_plot_data <- vector("list", length(unique(post_stt$site)))
 
-    stt_plot <- post_stt %>%
-        tidyr::pivot_longer(cols = c(mu_est, mu_lb, mu_ub),
-                            names_to = "quantile") %>%
-        ggplot() +
-        geom_path(aes(x = year, y = value, linetype = quantile)) +
-        geom_point(aes(x = year, y = count, col = factor(season)), show.legend = FALSE) +
-        scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
-        scale_colour_manual(values = plot_options$colors) +
-        # coord_cartesian(ylim = ylims) +
-        facet_wrap("season", ncol = 2,
-                   labeller = labeller(season = c("1" = "Summer", "2" = "Winter"))) +
-        xlab("Year") + ylab(abund_label) +
-        plot_options$pers_theme
+    for(i in seq_along(site_plot_data)){
+
+        # Abundance by season -----------------------------------------------------
+
+        stt_plot <- post_stt %>%
+            dplyr::filter(site == unique(post_stt$site)[i]) %>%
+            tidyr::pivot_longer(cols = c(mu_est, mu_lb, mu_ub),
+                                names_to = "quantile") %>%
+            ggplot() +
+            geom_path(aes(x = year, y = value, linetype = quantile)) +
+            geom_point(aes(x = year, y = count, col = factor(season)), show.legend = FALSE) +
+            scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
+            scale_colour_manual(values = plot_options$colors) +
+            # coord_cartesian(ylim = ylims) +
+            facet_wrap("season", ncol = 2,
+                       labeller = labeller(season = c("1" = "Summer", "2" = "Winter"))) +
+            xlab("Year") + ylab(abund_label) +
+            plot_options$pers_theme
 
 
-    # Trend plot --------------------------------------------------------------
+        # Trend plot --------------------------------------------------------------
 
-    # Create a data frame with the posterior trend
-    post_trd <- data.frame(beta_est = fit$mean$beta,
-                           beta_lb = fit$q2.5$beta,
-                           beta_ub = fit$q97.5$beta,
-                           prop_est = fit$mean$lambda,
-                           prop_lb = fit$q2.5$lambda,
-                           prop_ub = fit$q97.5$lambda,
-                           year = unique(ssm_counts$year))
+        # Create a data frame with the posterior trend
+        post_trd <- data.frame(beta_est = fit$mean$beta[i,],
+                               beta_lb = fit$q2.5$beta[i,],
+                               beta_ub = fit$q97.5$beta[i,],
+                               prop_est = fit$mean$lambda[i,],
+                               prop_lb = fit$q2.5$lambda[i,],
+                               prop_ub = fit$q97.5$lambda[i,],
+                               year = unique(ssm_counts$year))
 
-    if(linear){
-        post_trd <- post_trd %>%
-            dplyr::mutate(dplyr::across(.cols = -year,
-                                        .fns = ~exp(.x)))
-        rate_label <- "Growth rate"
-        ratio_label <- "W/S ratio"
+        if(linear){
+            post_trd <- post_trd %>%
+                dplyr::mutate(dplyr::across(.cols = -year,
+                                            .fns = ~exp(.x)))
+            rate_label <- "Growth rate"
+            ratio_label <- "W/S ratio"
 
-    } else {
+        } else {
 
-        rate_label <- "log growth rate"
-        ratio_label <- "log W/S ratio"
+            rate_label <- "log growth rate"
+            ratio_label <- "log W/S ratio"
+
+        }
+
+        # Plot trend
+        trd_plot <- post_trd %>%
+            dplyr::select(-dplyr::starts_with("prop")) %>%
+            tidyr::pivot_longer(cols = -year,
+                                names_to = "quantile") %>%
+            ggplot() +
+            geom_path(aes(x = year, y = value, linetype = quantile)) +
+            scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
+            scale_colour_manual(values = plot_options$colors) +
+            xlab("Year") + ylab(rate_label) +
+            plot_options$pers_theme
+
+
+        # Winter/summer ratio -----------------------------------------------------
+
+        # Plot proportion summer/winter
+        prop_plot <- post_trd %>%
+            dplyr::select(-dplyr::starts_with("beta")) %>%
+            tidyr::pivot_longer(cols = -year,
+                                names_to = "quantile") %>%
+            ggplot() +
+            geom_path(aes(x = year, y = value, linetype = quantile)) +
+            scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
+            scale_colour_manual(values = plot_options$colors) +
+            xlab("Year") + ylab(ratio_label) +
+            plot_options$pers_theme
+
+
+        # Save --------------------------------------------------------------------
+
+        # Title
+        plottitle <- paste(ifelse(unique(ssm_counts$spp) == "multi",
+                                  "Multiple species",
+                                  unique(ssm_counts$spp)),
+                           "at site",
+                           unique(ssm_counts$LocationCode)[i])
+
+        # To prevent opening multiple devices
+        pfile <- tempfile()
+        grDevices::png(pfile)
+        p <- gridExtra::grid.arrange(stt_plot, trd_plot, prop_plot,
+                                     layout_matrix = matrix(c(1,2,1,3), nrow = 2),
+                                     top = plottitle)
+        grDevices::dev.off()
+        unlink(pfile)
+
+        site_plot_data[[i]] <- list(plot = p,
+                                    data = list(post_stt %>%
+                                                    dplyr::filter(site == unique(post_stt$site)[i]), post_trd))
 
     }
 
-    # Plot trend
-    trd_plot <- post_trd %>%
-        dplyr::select(-dplyr::starts_with("prop")) %>%
-        tidyr::pivot_longer(cols = -year,
-                            names_to = "quantile") %>%
-        ggplot() +
-        geom_path(aes(x = year, y = value, linetype = quantile)) +
-        scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
-        scale_colour_manual(values = plot_options$colors) +
-        xlab("Year") + ylab(rate_label) +
-        plot_options$pers_theme
+    names(site_plot_data) <- unique(ssm_counts$LocationCode)
 
-
-    # Winter/summer ratio -----------------------------------------------------
-
-    # Plot proportion summer/winter
-    prop_plot <- post_trd %>%
-        dplyr::select(-dplyr::starts_with("beta")) %>%
-        tidyr::pivot_longer(cols = -year,
-                            names_to = "quantile") %>%
-        ggplot() +
-        geom_path(aes(x = year, y = value, linetype = quantile)) +
-        scale_linetype_manual(name = "", values = c(1, 2, 2), guide = NULL) +
-        scale_colour_manual(values = plot_options$colors) +
-        xlab("Year") + ylab(ratio_label) +
-        plot_options$pers_theme
-
-
-    # Save --------------------------------------------------------------------
-
-    # Title
-    plottitle <- ifelse(unique(ssm_counts$spp) == "multi",
-                        "Multiple species",
-                        unique(ssm_counts$spp))
-
-    # To prevent opening multiple devices
-    pfile <- tempfile()
-    grDevices::png(pfile)
-    p <- gridExtra::grid.arrange(stt_plot, trd_plot, prop_plot,
-                                 layout_matrix = matrix(c(1,2,1,3), nrow = 2),
-                                 top = plottitle)
-    grDevices::dev.off()
-    unlink(pfile)
-
-    return(list(plot = p,
-                data = list(post_stt, post_trd)))
+    return(site_plot_data)
 
 
 }
--- R/ppl_summarize_ssm.R
+++ R/ppl_summarize_ssm.R
@@ -7,89 +7,84 @@
 #' @export
 #'
 #' @examples
-ppl_summarize_ssm <- function(sp_code, site, year, config, ...){
+ppl_summarize_ssm <- function(sp_code, year, config, ...){
 
     print(paste("Summarizing state-space JAGS model at", Sys.time()))
 
     # Load counts
-    counts <- readRDS(file.path(config$data_dir, paste(site, year, "visit_covts.rds", sep = "_")))
+    counts <- utils::read.csv(file.path(config$out_dir, sp_code, paste0("abu_model_data_", sp_code, "_", config$years_ch,".csv")))
 
-    # Filter years
-    counts <- counts %>%
-        dplyr::filter(Year %in% config$years)
 
-    # Prepare data to fit an SSM
-    ssmcounts <- prepSsmData(counts = counts,
-                             spp_sel = if(sp_code == "all"){NULL} else {sp_code},
-                             keep = Hmisc::Cs(CountCondition, prcp, tmmn, tmmx, watext, watrec))
-
-    # Load fit
-    if(length(sp_code) > 1){
-        sp_code <- "group"
-    }
-
-    fit <- readRDS(file.path(config$data_outdir, sp_code, paste0("ssm_fit_", site, "_", config$years_ch, "_", sp_code, ".rds")))
+    fit <- readRDS(file.path(config$out_dir, sp_code, paste0("ssm_fit_", config$years_ch, "_", sp_code, ".rds")))
 
     # Plot
     pers_theme <- ggplot2::theme_bw()
-    p <- BIRDIE::plotSsm2ss(fit = fit, ssm_counts = ssmcounts, linear = TRUE,
+    p <- BIRDIE::plotSsm2ss(fit = fit, ssm_counts = counts, linear = TRUE,
                             plot_options = list(pers_theme = pers_theme,
                                                 colors = c("#71BD5E", "#B590C7")))
 
     # grid::grid.newpage()
     # grid::grid.draw(p$plot)
 
-    # Extract plot data
-    plotdata <- p$data
+    out_all_sites <- vector("list", length(unique(counts$LocationCode)))
 
-    # Prepare state data
-    stt_df <- plotdata[[1]]
+    for(i in seq_along(unique(counts$LocationCode))){
 
-    stt_dfw <- cbind(stt_df %>%
-                         dplyr::filter(season == 1) %>%
-                         dplyr::select(-season),
-                     stt_df %>%
-                         dplyr::filter(season == 2) %>%
-                         dplyr::select(-c(year, season)))
+        site_sel <- names(p)[[i]]
 
-    names(stt_dfw) <- c("summer.logest", "summer.logest.ci.lower", "summer.logest.ci.upper",
-                        "year", "log.summer.count",
-                        "winter.logest", "winter.logest.ci.lower", "winter.logest.ci.upper",
-                        "log.winter.count")
+        plotdir <- file.path(config$out_dir, sp_code, paste0("ssm_plot_", sp_code, "_", site_sel, "_", config$years_ch, ".png"))
+        ggplot2::ggsave(plotdir, plot = p[[i]]$plot, width = 6.4, height = 5.2)
 
-    # Prepare trend data
-    trd_df <- plotdata[[2]]
+        # Extract plot data
+        plotdata <- p[[i]]$data
 
-    names(trd_df) <- c("slope.est", "slope.ci.lower", "slope.ci.upper",
-                       "winter.prop.est", "winter.prop.ci.lower", "winter.prop.ci.upper",
-                       "year")
+        # Prepare state data
+        stt_df <- plotdata[[1]]
 
-    # Combine
-    out_df <- cbind(stt_dfw, dplyr::select(trd_df, -year))
+        stt_dfw <- cbind(stt_df %>%
+                             dplyr::filter(season == 1) %>%
+                             dplyr::select(-season),
+                         stt_df %>%
+                             dplyr::filter(season == 2) %>%
+                             dplyr::select(-c(year, season, site)))
 
-    # Add missing columns
-    out_df <- out_df %>%
-        dplyr::mutate(species = sp_code,
-                      summer.count = round(exp(log.summer.count)),
-                      winter.count = round(exp(log.winter.count)))
+        names(stt_dfw) <- c("year", "site", "summer.count", "summer.est", "summer.ci.lower", "summer.ci.upper",
+                            "winter.count", "winter.est", "winter.ci.lower", "winter.ci.upper")
 
-    # Order columns
-    out_df <- out_df %>%
-        dplyr::mutate(site = site) %>%
-        dplyr::select(site, species, year, summer.count, winter.count,
-                      log.summer.count, log.winter.count,
-                      summer.logest, winter.logest, winter.logest.ci.lower,
-                      winter.logest.ci.upper,
-                      summer.logest.ci.lower, summer.logest.ci.upper,
-                      slope.est, slope.ci.lower, slope.ci.upper,
-                      winter.prop.est, winter.prop.ci.lower,
-                      winter.prop.ci.upper)
+        # Prepare trend data
+        trd_df <- plotdata[[2]]
+
+        names(trd_df) <- c("slope.est", "slope.ci.lower", "slope.ci.upper",
+                           "winter.prop.est", "winter.prop.ci.lower", "winter.prop.ci.upper",
+                           "year")
+
+        # Combine
+        out_df <- cbind(stt_dfw, dplyr::select(trd_df, -year))
+
+        # Add missing columns
+        out_df <- out_df %>%
+            dplyr::mutate(species = sp_code)#,
+        # summer.count = round(exp(log.summer.count)),
+        # winter.count = round(exp(log.winter.count)))
+
+        # Order columns
+        out_df <- out_df %>%
+            dplyr::select(species, site, year, summer.count, winter.count,
+                          summer.est, winter.est, winter.ci.lower,
+                          winter.ci.upper,
+                          summer.ci.lower, summer.ci.upper,
+                          slope.est, slope.ci.lower, slope.ci.upper,
+                          winter.prop.est, winter.prop.ci.lower,
+                          winter.prop.ci.upper)
+
+        out_all_sites[[i]] <- out_df
+
+    }
+
+    out_all_sites <- dplyr::bind_rows(out_all_sites)
 
     # Export sample
-    datadir <- file.path(config$data_outdir, sp_code, paste0("ssm_pred_", site, "_", config$years_ch, "_", sp_code, ".csv"))
+    datadir <- file.path(config$out_dir, sp_code, paste0("ssm_pred_", sp_code, "_", site, "_", config$years_ch, ".csv"))
     utils::write.csv(out_df, datadir, row.names = FALSE)
-
-    plotdir <- file.path(config$data_outdir, sp_code, paste0("ssm_plot_", site, "_", config$years_ch, "_", sp_code, ".png"))
-    ggplot2::ggsave(plotdir, plot = p$plot, width = 6.4, height = 5.2)
 
 }
