442762e7e78030086dd555bf1b8b8e0d79e1b7a0
patchcervan <francerper@gmail.com>
2022-06-03 14:59:36 +0200

    Removed update file from create indicator file

--- R/ppl_create_indtr_file.R
+++ R/ppl_create_indtr_file.R
@@ -4,15 +4,12 @@
 #' @param year Year to run to the pipeline for
 #' @param overwrite_indtr Logical. If TRUE, existing files in directories
 #' corresponding to the species in config$species will be overwritten.
-#' @param update_file A character string with the file path to a previous indicator
-#' file that should serve as a basis for the new indicator file. Defaults to NULL,
-#' and so a clean new file would be created.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-ppl_create_indtr_file <- function(sp_code, year, overwrite_indtr, update_file = NULL){
+ppl_create_indtr_file <- function(sp_code, year, overwrite_indtr){
 
     # Create indicator file path
     indtr_file <- file.path(config$out_dir, sp_code, paste0("indtr_dst_", sp_code, "_", year, ".csv"))
@@ -20,25 +17,17 @@ ppl_create_indtr_file <- function(sp_code, year, overwrite_indtr, update_file = 
     # Check if file exists
     if(!file.exists(indtr_file) | (file.exists(indtr_file) & overwrite_indtr)){
 
-        if(is.null(update_file)){
-
-            # Create empty data frame
-            indtr <- data.frame(species = character(),
-                                indicator = character(),
-                                start_date = character(),
-                                end_date = character(),
-                                term = character(),
-                                estimate = numeric(),
-                                st_dev = numeric(),
-                                lb95 = numeric(),
-                                ub95 = numeric(),
-                                opt = numeric())
-
-        } else {
-
-            indtr <- read.csv(update_file)
-
-        }
+        # Create empty data frame
+        indtr <- data.frame(species = character(),
+                            indicator = character(),
+                            start_date = character(),
+                            end_date = character(),
+                            term = character(),
+                            estimate = numeric(),
+                            st_dev = numeric(),
+                            lb95 = numeric(),
+                            ub95 = numeric(),
+                            opt = numeric())
 
         # Save
         write.csv(indtr, indtr_file, row.names = FALSE)
