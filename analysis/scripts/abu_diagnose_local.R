ff3dd2d573ecfbfe69960a3e087b7d4542cad5f2
patchcervan <francerper@gmail.com>
2022-07-12 12:31:44 +0200

    Added script for basic Rhat diagnotics for abu models

---
+++ analysis/scripts/abu_diagnose_local.R
@@ -0,0 +1,32 @@
+library(BIRDIE)
+
+rm(list = ls())
+
+config <- BIRDIE::configPreambJAGS(2017, server = FALSE)
+
+rhat_df <- data.frame()
+
+for(i in 1:length(config$species)){
+
+    sp_code <- config$species[i]
+
+    print(paste0("Working on species ", sp_code, " (", i, " of ", length(config$species), ")"))
+
+    fit <- readRDS(file.path("analysis/out_nosync", sp_code, paste0("ssm_fit_", config$years_ch, "_", sp_code, ".rds")))
+
+    names(fit$Rhat)
+
+    rhats <- lapply(fit$Rhat, function(x) sum(x > 1.09)) %>%
+        as.data.frame()
+
+    rhats$sp <- sp_code
+    rhats$nobs <- length(fit$mean$mu_t)
+    rhats$years <- config$years_ch
+
+    rhat_df <- rbind(rhat_df, rhats)
+
+}
+
+write.csv(rhat_df,
+          file.path(config$out_dir, paste0("abu_diag_", config$years_ch, "_", sp_code, ".csv")),
+          row.names = FALSE)
