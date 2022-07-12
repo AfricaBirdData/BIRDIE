3faea39f22b868d5a2587a34085e8a472f24bcb5
patchcervan <francerper@gmail.com>
2022-07-12 15:47:17 +0200

    Fixed sinked message for lack of sites

--- R/ppl_create_data_ssm.R
+++ R/ppl_create_data_ssm.R
@@ -205,7 +205,7 @@ ppl_create_data_ssm <- function(sp_code, year, catchment, config,
         # If less than 5 sites create notification, because we might need a different model
         if(length(sites_good) < 5){
 
-            sink(file.path(config$out_dir, sp_code, paste0("Less_5_sites_", sp_code, "_", config$year_ch, ".txt")))
+            sink(file.path(config$out_dir, sp_code, paste0("Less_5_sites_", sp_code, "_", config$years_ch, ".txt")))
             message(paste("Less than 5 sites for species", sp_code, "on year", config$year))
             sink()
 
