5311073bafa65a7b20d8d65ace1c206eb989ad1b
patchcervan <francerper@gmail.com>
2022-07-27 15:13:43 +0200

    Mean reverting process

--- analysis/models/cwac_ssm_lat_season_multi_hier.R
+++ analysis/models/cwac_ssm_lat_season_multi_hier.R
@@ -4,6 +4,9 @@
 # State changes are estimated in a hierarchical way such that changes across sites
 # in a given year come from a common distribution.
 
+# NOTE THAT THE MODEL ASSUMES COUNTS ARE ADDED ONE TO AVOID ERO COUNTS AND NON-IDENTIFIBILITY OF PARAMETERS.
+# SEE RE-DEFINITION OF MU_T_1 INTO MU_T AT THE END OF THE MODEL
+
 model {
 
     ### PRIORS: ###
@@ -11,12 +14,16 @@ model {
     for(s in 1:nsites){
 
         # Priors for initial states
-        stt_s[s, 1] ~ dnorm(0, 1/10)       # prior for initial log population size
-        beta[s, 1] ~ dnorm(0, 1/10)        # prior for initial log growth rate
-        lambda[s, 1] ~ dnorm(0, 1/10)      # prior for log summer to winter ratio
+        stt_s[s, 1] ~ dnorm(0, 1/10)              # prior for initial log population size
+        beta[s, 1] ~ dnorm(0, 1)                  # prior for initial log growth rate
+        lambda[s, 1] ~ dnorm(0, 1/10)             # prior for log summer to winter ratio
+        stt_w[s, 1] = stt_s[s, 1] + lambda[s, 1]  # This is not a prior but inherits directly from two priors
 
-        tau.alpha[s] ~ dgamma(2, 2)
-        tau.e[s] ~ dgamma(2, 2)
+        mu.beta[s] ~ dnorm(0, 1)        # prior for mean log population rate of change over time (long-term trend)
+        phi[s] ~ dunif(0, 1)               # prior for mean-reverting parameter
+
+        tau.alpha[s] ~ dgamma(2, 0.5)
+        tau.e[s] ~ dgamma(2, 0.5)
 
         sig.alpha[s] = sqrt(1/tau.alpha[s])   # observer error standard deviation summer
         sig.e[s] = sqrt(1/tau.e[s])           # observer error standard deviation winter
@@ -30,40 +37,60 @@ model {
 
     ### latent states ###
 
-    # State updates
+    # Update population changes. Note that we need more lambdas than betas. This
+    # because we need to estimate winter to summer proportion for every year, but
+    # we don't estimate population change for the last year.
+
+    # For update lambda in the second year
+    tau.eps[1] ~ dscaled.gamma(2, 2)     # same as Student-t with 2 degrees of freedom and standard deviation 5 for sig.eps!
+    # tau.eps[y] ~ dgamma(1.8, 1)
+    sig.eps[1] = sqrt(1/tau.eps[1])
+
     for(s in 1:nsites){
-        stt_w[s, 1] = stt_s[s, 1] + lambda[s, 1]
+
+        # Sample eps
+        eps[s, 1] ~ dnorm(0, tau.eps[1])
+
+        # Lambda update
+        lambda[s, 2] = lambda[s, 1] + eps[s, 1]                          # winter to summer ratio
+
     }
 
-    for (y in 1:(nyears-1)){
+    # For the rest of population changes updates
+    for(y in 1:(nyears - 2)){
 
-        # tau.zeta[y] ~ dscaled.gamma(5, 2)    # same as Student-t with 2 degrees of freedom and standard deviation 5 for sig.zeta!
-        tau.zeta[y] ~ dgamma(1.8, 1)
+        tau.zeta[y] ~ dscaled.gamma(3, 2)    # same as Student-t with 2 degrees of freedom and standard deviation 5 for sig.zeta!
+        # tau.zeta[y] ~ dgamma(1.8, 1)
         sig.zeta[y] = sqrt(1/tau.zeta[y])
 
-        tau.eps[y] ~ dscaled.gamma(5, 2)     # same as Student-t with 2 degrees of freedom and standard deviation 5 for sig.eps!
+        tau.eps[y+1] ~ dscaled.gamma(3, 2)     # same as Student-t with 2 degrees of freedom and standard deviation 5 for sig.eps!
         # tau.eps[y] ~ dgamma(1.8, 1)
-        sig.eps[y] = sqrt(1/tau.eps[y])
+        sig.eps[y+1] = sqrt(1/tau.eps[y+1])
 
         for(s in 1:nsites){
 
             # Sample zeta and eps
             zeta[s, y] ~ dnorm(0, tau.zeta[y])
-            eps[s, y] ~ dnorm(0, tau.eps[y])
+            eps[s, y+1] ~ dnorm(0, tau.eps[y+1])
 
             # Beta update
-            # w[s, y] ~ dnorm(0, tau.w[s])
-            beta[s, y+1] = beta[s, y] + zeta[s, y]
+            beta[s, y+1] = beta[s, y] + phi[s]*(mu.beta[s] - beta[s, y]) + zeta[s, y]       # summer population change
 
             # Lambda update
-            lambda[s, y+1] = lambda[s, y] + eps[s, y] # winter to summer ratio
+            lambda[s, y+2] = lambda[s, y+1] + eps[s, y+1]                      # winter to summer ratio
+
+        }
+    }
+
+    # Update population states
+    for (y in 1:(nyears-1)){
+        for(s in 1:nsites){
 
             # State update
-            stt_s[s, y+1] = stt_s[s, y] + beta[s, y+1]# + w[s, y]
+            stt_s[s, y+1] = stt_s[s, y] + beta[s, y]# + w[s, y]
             stt_w[s, y+1] = stt_s[s, y+1] + lambda[s, y+1]
 
         }
-
     }
 
     # model for summer/winter
@@ -78,13 +105,17 @@ model {
     for(i in 1:N){
 
         # State process
-        mu_t[i] = stt_s[site[i], year[i]]*summer[i] + stt_w[site[i], year[i]]*winter[i]
+        mu_t_1[i] = stt_s[site[i], year[i]]*summer[i] + stt_w[site[i], year[i]]*winter[i]
 
         # observation process
-        obs[i] ~ dnorm(mu_t[i], tau.alpha[site[i]]*summer[i] + tau.e[site[i]]*winter[i])
+        obs[i] ~ dnorm(mu_t_1[i], tau.alpha[site[i]]*summer[i] + tau.e[site[i]]*winter[i])
 
         count[i] ~ dpois(exp(obs[i]))
 
     }
+
+    # Prepare mu_t_1 for export: SUBTRACT 1 BECAUSE WE ADDED 1 TO ALL COUNTS
+    # BETAS AND LAMBDAS SHOULD BE UNAFFECTED BY THIS
+    mu_t = mu_t_1 - 1
 
 }
