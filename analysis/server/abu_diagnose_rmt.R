0bb01f1255d71980e304e2509fa94550577472d5
patchcervan <francerper@gmail.com>
2022-08-01 13:48:46 +0200

    Updated remote diagnose file with filepath setup and Rhat diff

--- analysis/server/abu_diagnose_rmt.R
+++ analysis/server/abu_diagnose_rmt.R
@@ -10,10 +10,10 @@ for(i in 1:length(config$species)){
 
     sp_code <- config$species[i]
 
-    message(paste0("Working on species ", sp_code, " (", i, " of ", length(config$species), ")"))
+    print(paste0("Working on species ", sp_code, " (", i, " of ", length(config$species), ")"))
 
     # Skip species if less than 5 suitable sites were detected during fitting model fitting
-    error_file <- file.path(config$out_dir, sp_code, paste0("Less_5_sites_", sp_code, "_", config$years_ch, ".txt"))
+    error_file <- setSpOutFilePath("Less_5_sites", config, sp_code, ".txt")
 
     if(file.exists(error_file)){
         message(paste0("Less_5_sites_", sp_code, "_", config$years_ch, ".txt"))
@@ -21,15 +21,15 @@ for(i in 1:length(config$species)){
     }
 
     # Else proceed with diagnostics
-    fit <- readRDS(file.path(config$out_dir, sp_code, paste0("ssm_fit_", config$years_ch, "_", sp_code, ".rds")))
+    fit <- readRDS(setSpOutFilePath("ssm_fit", config, sp_code, ".rds"))
 
-    names(fit$Rhat)
+    fit_stats <- BIRDIE:::processJAGSoutput(fit, DIC = FALSE, params.omit = NULL)
 
-    rhats <- lapply(fit$Rhat, function(x) sum(x > 1.09)) %>%
+    rhats <- lapply(fit_stats$Rhat, function(x) sum(abs(x - 1) > 0.1)) %>%
         as.data.frame()
 
     rhats$sp <- sp_code
-    rhats$nobs <- length(fit$mean$mu_t)
+    rhats$nobs <- length(fit_stats$mean$mu_t)
     rhats$years <- config$years_ch
 
     rhat_df <- rbind(rhat_df, rhats)
@@ -37,5 +37,5 @@ for(i in 1:length(config$species)){
 }
 
 write.csv(rhat_df,
-          file.path(config$out_dir, paste0("abu_diag_", config$years_ch, "_", sp_code, ".csv")),
+          file.path(config$out_dir, paste0("abu_diag_", config$years_ch, ".csv")),
           row.names = FALSE)
