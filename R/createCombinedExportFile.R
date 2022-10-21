b3862145bbb4140e4869cbf7d230f26f9f7d82bc
patchcervan <francerper@gmail.com>
2022-10-21 14:39:46 +0200

    Created file to create combined export files

---
+++ R/createCombinedExportFile.R
@@ -0,0 +1,76 @@
+#' Create a combined file for export to data mart
+#'
+#' @param config A list with pipeline configuration parameters.
+#' See \link{configPreambJAGS} and \link{configPreambOccuR}
+#' @param type A character string with three options: "abu", for abundance estimates files,
+#' "dst" for occupancy estimates files, or "indtr_dst" for distribution indicator files.
+#'
+#' @return It will create a file in config$out_dir combining all files of the selected
+#' type of all species and years in config$years.
+#' @export
+#'
+#' @examples
+createCombinedExportFile <- function(config, type = c("abu", "dst", "indtr_dst")){
+
+    if(type == "abu"){
+
+        abu_out <- data.frame()
+
+        for(i in 1:length(config$species)){
+
+            sp_code <- config$species[i]
+
+            abu_file <- file.path(config$out_dir, sp_code, paste0("ssm_pred_", config$years_ch, "_", sp_code, "_all.csv"))
+
+            abu_out <- rbind(abu_out, read.csv(abu_file))
+
+        }
+
+        saveRDS(abu_out, file.path(config$out_dir, paste0("ssm_pred_", config$years_ch, "_all_all.csv")))
+    }
+
+    if(type == "dst"){
+
+        dst_out <- data.frame()
+
+        for(i in 1:length(config$species)){
+
+            sp_code <- config$species[i]
+
+            for(y in 1:length(config$years)){
+
+                yr <- substring(as.character(config$years[y]), 3, 4)
+                dst_file <- file.path(config$out_dir, sp_code, paste0("occur_pred_", yr, "_", sp_code, ".csv"))
+                dst_out <- rbind(dst_out, read.csv(dst_file))
+
+            }
+
+        }
+
+        saveRDS(dst_out, file.path(config$out_dir, paste0("occur_pred_", config$years_ch, "_all.csv")))
+    }
+
+    if(type == "indtr_dst"){
+
+        dst_out <- data.frame()
+
+        for(i in 1:length(config$species)){
+
+            sp_code <- config$species[i]
+
+            for(y in 1:length(config$years)){
+
+                yr <- config$years[y]
+                dst_file <- file.path(config$out_dir, sp_code, paste0("indtr_dst_", sp_code, "_", yr, ".csv"))
+                dst_out <- rbind(dst_out, read.csv(dst_file))
+
+            }
+
+        }
+
+        saveRDS(dst_out, file.path(config$out_dir, paste0("indtr_dst_all_", config$years_ch, ".csv")))
+    }
+
+
+}
+
