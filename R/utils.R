3cfe4456353a98fc357cf3262346baa983b7bb8e
patchcervan <francerper@gmail.com>
2022-12-13 16:46:05 +0200

    Created utils function to run occupancy in parallel

---
+++ R/utils.R
@@ -0,0 +1,42 @@
+#' @export
+#' @example
+#' \dontrun{}
+#'
+#'
+#' Fit occupancy models in parallel
+#'
+#' @description Wrapper around ppl_run_pipe_dst1 (fit step) for furrr parallel calls
+#' @param sp_code Species SAFRING code
+#' @param .config Config object from \link{configPreambOccu}
+#'
+#' @keywords internal
+#' @noRd
+pipe_prll_fit <- function(sp_code, .config){
+
+    # Species name
+    sp_name <- BIRDIE::barberspan %>%
+        dplyr::filter(SppRef == sp_code) %>%
+        dplyr::mutate(name = paste(Common_species, Common_group)) %>%
+        dplyr::mutate(name = gsub(" NA|NA ", "", name)) %>% # in case there are NAs in species or group
+        dplyr::pull(name) %>%
+        unique()
+
+    # Pipeline module 1
+    out_dst1 <- ppl_run_pipe_dst1(sp_code = sp_code,
+                                  sp_name = sp_name,
+                                  year = year,
+                                  config = .config,
+                                  steps = c("data", "fit"),
+                                  force_gee_dwld = FALSE,
+                                  force_abap_dwld = FALSE,
+                                  save_occu_data = TRUE,
+                                  overwrite_occu_data = c("det"),
+                                  scale_vars_occur = list(visit = NULL,
+                                                          site = c("dist_coast", "prcp", "tdiff", "ndvi", "watext", "watrec")),
+                                  print_fitting = FALSE,
+                                  verbose = TRUE,
+                                  monitor = FALSE)
+
+    message(paste("Pipeline DST1 status =", out_dst1))
+
+}
--- analysis/scripts/pipeline_distr_local_parall.R
+++ analysis/scripts/pipeline_distr_local_parall.R
@@ -8,21 +8,22 @@ test_years <- c(2012, 2013, 2014, 2015)
 run_modules <- 1
 ncores <- 2
 parall <- ncores != 1
+annotate <- FALSE
 
 for(y in seq_along(test_years)){
 
     year <- test_years[y]
     config <- configPreambOccu(year = year, dur = 3, dim_grid = 10, server = FALSE)
 
-    # Create aux variables for parallel computing
+    # Create aux indices for parallel computing
     if(parall){
-        seq_spp <- seq_along(config$species)[-1]
-        keep <- c(1, seq(min(seq_spp), max(seq_spp), ncores))
+        seq_spp <- seq_along(config$species)
+        keep <- seq(min(seq_spp), max(seq_spp), ncores)
 
         if((length(seq_spp) %% ncores) == 0){
-            ppll <- c(FALSE, rep(TRUE, length(keep)-1))
+            ppll <- rep(TRUE, length(keep))
         } else {
-            ppll <- c(FALSE, rep(TRUE, length(keep)-2), FALSE)
+            ppll <- c(rep(TRUE, length(keep)-1), FALSE)
         }
     } else {
         keep <- seq_along(config$species)
@@ -36,71 +37,28 @@ for(y in seq_along(test_years)){
         j <- ppll[i]
         sp_codes <- config$species[idx:(idx+j)]
 
-        message(paste0("Working on species ", paste(sp_codes, collapse = ", "), " (", i, " of ", length(keep), ")"))
+        # Annotate data if necessary
+        if(annotate && i == 1){
 
-        pipe_fun <- function(sp_code, .run_modules=run_modules, .config=config, .i=i){
+            message(paste0("Annotating sites/visits species ", paste(sp_codes[1], collapse = ", "), " (", i, " of ", length(keep), ")"))
 
-            # Species name
-            sp_name <- BIRDIE::barberspan %>%
-                dplyr::filter(SppRef == sp_code) %>%
-                dplyr::mutate(name = paste(Common_species, Common_group)) %>%
-                dplyr::mutate(name = gsub(" NA|NA ", "", name)) %>% # in case there are NAs in species or group
-                dplyr::pull(name) %>%
-                unique()
-
-            # Pipeline module 1
-            if(1 %in% .run_modules){
-
-                out_dst1 <- ppl_run_pipe_dst1(sp_code = sp_code,
-                                              sp_name = sp_name,
-                                              year = year,
-                                              config = .config,
-                                              steps = c("data", "fit"),
-                                              force_gee_dwld = FALSE,
-                                              force_abap_dwld = FALSE,
-                                              save_occu_data = TRUE,
-                                              overwrite_occu_data = c("site", "visit", "det"),
-                                              scale_vars_occur = list(visit = NULL,
-                                                                      site = c("dist_coast", "prcp", "tdiff", "ndvi", "watext", "watrec")),
-                                              print_fitting = FALSE,
-                                              verbose = TRUE,
-                                              monitor = TRUE)
-
-                message(paste("Pipeline DST1 status =", out_dst1))
-
-                if(out_dst1 == 1){
-                    next
-                }
-
-            }
-
-            # Pipeline module 2
-            if(2 %in% .run_modules){
-
-                out_dst2 <- ppl_run_pipe_dst2(sp_code = sp_code,
-                                              config = .config,
-                                              indtr = c("aoo", "daoo"),
-                                              create = TRUE,
-                                              overwrite_indtr = TRUE,
-                                              verbose = TRUE,
-                                              scale_vars_occur = list(visit = NULL,
-                                                                      site = c("dist_coast", "prcp", "tdiff", "ndvi", "watext", "watrec")))
-
-                message(paste("Pipeline DST2 status =", out_dst2))
-
-                if(out_dst2 == 1){
-                    next
-                }
-
-            }
-
+            ppl_create_site_visit(sp_code = sp_codes[1],
+                                  year = year,
+                                  force_gee_dwld = TRUE,
+                                  save_occu_data = TRUE,
+                                  overwrite_occu_data = c("site", "visit", "det"),
+                                  config = config,
+                                  force_abap_dwld = TRUE,
+                                  monitor = TRUE)
         }
+
+        message(paste0("Working on species ", paste(sp_codes, collapse = ", "), " (", i, " of ", length(keep), ")"))
 
         if(parall){
             future::plan("multisession", workers = ncores)
         }
 
-        furrr::future_map(sp_codes, ~pipe_fun(.x))
+        furrr::future_map(sp_codes, ~pipe_prll_fit(.x, config))
 
         if(parall){
             future::plan("sequential")
