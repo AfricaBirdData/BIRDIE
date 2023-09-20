7f2f5e12ba71a2033341eb050557de42833b113b
patchcervan <francerper@gmail.com>
2023-09-20 09:41:40 +0200

    Unified scripts to create path lists

--- analysis/scripts/create_path_list.R
+++ analysis/scripts/create_path_list.R
@@ -4,29 +4,37 @@ rm(list = ls())
 
 # Set the parent directory where all model fits can be found.
 # The parent directory needs to host all of the species directories
-files_dir <- "/drv_birdie/birdie_ftp"
+files_dir <- "analysis/out_nosync"
 
 # Set the directory where the file with the model fit names is stored
-out_dir <- "/home/birdie/analysis"
+out_dir <- "analysis/out_nosync"
+
+# Define the pattern we want to find. It can contain regular expressions
+tofind <- "^occu_diagnostics_spOccupancy_"
 
 # Extract file names without full path to handle more easily
 ff <- list.files(path = files_dir,
-                 pattern = "^occu_fit",
+                 pattern = tofind,
                  full.names = FALSE,
                  recursive = TRUE)
 
 # Separate directory (species) from file name and create a data frame
 ff_split <- strsplit(ff, "/")
 
-
 ff_df <- data.frame(sp_code = sapply(ff_split, "[[", 1),
-                    file = sapply(ff_split, "[[", 2)) %>%
-    mutate(year = gsub("occu_fit_spOccupancy_", "", file)) %>%
-    mutate(year = gsub("\\_.*", "", year)) %>%
+                    file = sapply(ff_split, "[[", 2))
+
+# THE SECTIONS BELOW NEED TO BE MODIFIED ACCORDING TO THE ACTUAL FILE PATHS IN
+# THE DATAFRAME AND WHAT WE WANT TO ACHIEVE.
+
+# In this example we want to isolate the year to be able to filter by that field
+ff_df <- ff_df %>%
+    mutate(year = gsub(tofind, "", file)) %>%
+    mutate(year = gsub("\\_.*\\_ZA.csv", "", year)) %>%
     mutate(year = as.integer(year)) %>%
     mutate(file_full = paste(sp_code, file, sep =  "/"))
 
-# Extract the last model fit per species
+# Extract the last diagnostic file for each species
 keep <- ff_df %>%
     group_by(sp_code) %>%
     filter(year == max(year)) %>%
@@ -36,7 +44,7 @@ keep <- ff_df %>%
 
 # If we needed the full paths of the files (we tipically don't for transfers)
 ff_full <- list.files(path = files_dir,
-                      pattern = "^occu_fit",
+                      pattern = tofind,
                       full.names = TRUE,
                       recursive = TRUE)
 
@@ -44,4 +52,4 @@ ff_full <- list.files(path = files_dir,
 # Select files to keep (change to ff_full if needed)
 ff_keep <- ff[ff %in% keep]
 
-writeLines(ff_keep, con = file.path(out_dir, "files_to_keep.txt"))
+writeLines(ff_keep, con = file.path(out_dir, "path_list.txt"))
