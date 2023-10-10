0258c3df5ed070270b3db3fc3fa837886447f843
patchcervan <francerper@gmail.com>
2023-10-10 14:54:49 +0200

    Added section to merge environmental layers

--- analysis/scripts/merge_pipeline_outputs.R
+++ analysis/scripts/merge_pipeline_outputs.R
@@ -2,7 +2,7 @@ library(BIRDIE)
 
 rm(list = ls())
 
-modules <- c("abu", "dst")
+modules <- c("abu", "dst", "env")
 
 
 # Merge abundance estimates -----------------------------------------------
@@ -43,5 +43,55 @@ if("dst" %in% modules){
 
     # Merge and save in exports
     createCombinedExportFile(config, type = "dst")
+
+}
+
+
+# Merge environmental data ------------------------------------------------
+
+if("env" %in% modules){
+
+    # Configure
+    config <- configPipeline(
+        year = 2022,
+        dur = 14,
+        package = "spOccupancy",
+        data_dir = NULL,
+        out_dir = NULL,
+        server = TRUE
+    )
+
+    # Model period (years)
+    years_per_file <- 3
+
+    periods <- with(config, seq(year - dur, year, years_per_file))
+    periods <- paste(periods, periods+years_per_file-1, sep = "_")
+    periods <- gsub("^.{0,2}|\\_.{0,2}", "_", periods)
+
+    # Merge and save in exports
+    fpaths <- file.path(config$out_dir, paste0("site_dat_sa_gee", periods, ".csv"))
+
+    env_out <- data.frame()
+
+    for(i in 1:length(fpaths)){
+
+        env_file <- fpaths[i]
+
+        if(file.exists(env_file)){
+
+            new_env <- utils::read.csv(env_file) %>%
+                dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ round(.x, 3)))
+
+            env_out <- dplyr::bind_rows(env_out, new_env)
+
+        } else {
+            env_out <- env_out
+        }
+
+    }
+
+    utils::write.csv(env_out,
+                     setSpOutFilePath("site_dat_gee", config, config$years_ch, "export", ".csv"),
+                     row.names = FALSE)
 
 }
