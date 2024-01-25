52b7145bf0a15f2dafc4325fc77c9f596b9efdab
patchcervan <francerper@gmail.com>
2024-01-25 07:55:01 +0100

    Script handles different level paths

--- analysis/scripts/create_path_list.R
+++ analysis/scripts/create_path_list.R
@@ -4,13 +4,13 @@ rm(list = ls())
 
 # Set the parent directory where all model fits can be found.
 # The parent directory needs to host all of the species directories
-files_dir <- "analysis/out_nosync"
+files_dir <- "/drv_birdie/birdie_ftp"
 
 # Set the directory where the file with the model fit names is stored
-out_dir <- "analysis/out_nosync"
+out_dir <- "/home/birdie/analysis"
 
 # Define the pattern we want to find. It can contain regular expressions
-tofind <- "^occu_diagnostics_spOccupancy_"
+tofind <- "^occu_site_dat_"
 
 # Extract file names without full path to handle more easily
 ff <- list.files(path = files_dir,
@@ -19,37 +19,45 @@ ff <- list.files(path = files_dir,
                  recursive = TRUE)
 
 # Separate directory (species) from file name and create a data frame
-ff_split <- strsplit(ff, "/")
+if(any(grep("/", ff))){
 
-ff_df <- data.frame(sp_code = sapply(ff_split, "[[", 1),
-                    file = sapply(ff_split, "[[", 2))
+    ff_split <- strsplit(ff, "/")
 
-# THE SECTIONS BELOW NEED TO BE MODIFIED ACCORDING TO THE ACTUAL FILE PATHS IN
-# THE DATAFRAME AND WHAT WE WANT TO ACHIEVE.
+    ff_df <- data.frame(sp_code = sapply(ff_split, "[[", 1),
+                        file = sapply(ff_split, "[[", 2))
 
-# In this example we want to isolate the year to be able to filter by that field
-ff_df <- ff_df %>%
-    mutate(year = gsub(tofind, "", file)) %>%
-    mutate(year = gsub("\\_.*\\_ZA.csv", "", year)) %>%
-    mutate(year = as.integer(year)) %>%
-    mutate(file_full = paste(sp_code, file, sep =  "/"))
+    # THE SECTIONS BELOW NEED TO BE MODIFIED ACCORDING TO THE ACTUAL FILE PATHS IN
+    # THE DATAFRAME AND WHAT WE WANT TO ACHIEVE.
 
-# Extract the last diagnostic file for each species
-keep <- ff_df %>%
-    group_by(sp_code) %>%
-    filter(year == max(year)) %>%
-    ungroup() %>%
-    pull(file_full)
+    # In this example we want to isolate the year to be able to filter by that field
+    ff_df <- ff_df %>%
+        mutate(year = gsub(tofind, "", file)) %>%
+        mutate(year = gsub("\\_.*\\_ZA.csv", "", year)) %>%
+        mutate(year = as.integer(year)) %>%
+        mutate(file_full = paste(sp_code, file, sep =  "/"))
 
-
-# If we needed the full paths of the files (we tipically don't for transfers)
-ff_full <- list.files(path = files_dir,
-                      pattern = tofind,
-                      full.names = TRUE,
-                      recursive = TRUE)
+    # Extract the last diagnostic file for each species
+    keep <- ff_df %>%
+        group_by(sp_code) %>%
+        #filter(year == max(year)) %>%
+        ungroup() %>%
+        pull(file_full)
 
 
-# Select files to keep (change to ff_full if needed)
-ff_keep <- ff[ff %in% keep]
+    # If we needed the full paths of the files (we tipically don't for transfers)
+    ff_full <- list.files(path = files_dir,
+                          pattern = tofind,
+                          full.names = TRUE,
+                          recursive = TRUE)
+
+
+    # Select files to keep (change to ff_full if needed)
+    ff_keep <- ff[ff %in% keep]
+
+} else {
+
+    ff_keep <- ff
+
+}
 
 writeLines(ff_keep, con = file.path(out_dir, "path_list.txt"))
